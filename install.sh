98e1918325865b0cb46819e03dd383c7492c348c
Emmanuel Leon GM <44347338+GomesGokuBR@users.noreply.github.com>
2023-07-31 17:12:40 +0200

    Remove unused libs and fix syntax

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ clear && apt install -y i3-wm picom unclutter connman
 clear && apt-get install -y sddm --no-install-recommends
 
 # Packages installation [Dev]
-clear && apt install -y build-essential module-assistant fakeroot kernel-package libncurses5-dev
-clear && apt install -y install libgtk-3-0 libblkid1 liblzma5 libbluetooth-dev glibc-source
+clear && apt install -y build-essential module-assistant fakeroot libncurses5-dev
+clear && apt install -y libgtk-3-0 libblkid1 liblzma5 libbluetooth-dev glibc-source
 
 # Packages installation [intel]
 clear && apt install -y xserver-xorg-video-intel
